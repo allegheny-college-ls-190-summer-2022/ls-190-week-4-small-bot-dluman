aefb4e4f733d5600a8dd470fdefb7d5cc4e5e46e
dluman <dluman@allegheny.edu>
2022-07-19 09:43:44 -0400

    Updating with zodiac bot changes

--- src/twitter_bot.py
+++ src/twitter_bot.py
@@ -1,5 +1,7 @@
+import datetime
 import json
 import random
+import tweepy
 
 def load_keys():
     """Load keys file"""
@@ -27,7 +29,22 @@ def tweet(message):
     # Post message
     api.update_status(message)
     
+def get_year():
+    now = datetime.datetime.now()
+    date = now.date()
+    return date.strftime("%Y")
+    
 def main():
+    #         File path,                      File mode
+    fh = open("_data/divination/zodiac.json", "r")
+    zodiac = json.load(fh)
+    #     dictionary --> associative array (key => value)
+    eastern_zodiac = zodiac["eastern_zodiac"]
+    for zodiac in eastern_zodiac:
+        zodiac_year = eastern_zodiac[zodiac]["years"]
+        if get_year() not in zodiac_year:
+            tweet(f"It is not the year of the {zodiac}")
+            break
     # TODO: Implement your bot in the main function
     #       provided here and dont forget to tweet!
     pass    # <-- delete or comment this line out when done
